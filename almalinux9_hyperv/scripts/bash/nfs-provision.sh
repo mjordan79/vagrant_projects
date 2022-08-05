bca40a2733c68b72f46906f0a0cfcc8b8d6e695b
rperini <renato.perini@objectway.com>
2022-08-05 23:13:30 +0200

    Added NFS Client and Server provisioning

---
+++ almalinux9_hyperv/scripts/bash/nfs-provision.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NO_NODE=$1
+
+echo "NFS Provisioning."
+if [ $NO_NODE -eq 1 ] 
+then
+    echo "I'm on the first node... Configuring the NFS server ..."
+    dnf install -yq nfs-utils
+    mkdir -p /nfs/data
+    mkdir -p /nfs/logs
+    chown -R nobody:nobody /nfs
+    echo "/nfs/data  *(rw,sync,no_all_squash,root_squash)" > /etc/exports
+    echo "/nfs/logs  *(rw,sync,no_all_squash,root_squash)" >> /etc/exports
+    systemctl enable --now nfs-server
+    exportfs -arv
+else
+    echo "I'm on node 192.169.0.1$NO_NODE ... Configuring NFS client"
+    dnf install -yq nfs-utils nfs4-acl-tools
+    showmount -e 192.169.0.11
+    mkdir -p /mnt/nfs/data
+    mkdir -p /mnt/nfs/logs
+   
+    mount -t nfs  192.169.0.11:/nfs/data /mnt/nfs/data
+    mount -t nfs  192.169.0.11:/nfs/logs /mnt/nfs/logs
+    echo "192.169.0.11:/nfs/data     /mnt/nfs/data  nfs     defaults 0 0" >> /etc/fstab
+    echo "192.169.0.11:/nfs/logs     /mnt/nfs/logs  nfs     defaults 0 0" >> /etc/fstab
+fi
