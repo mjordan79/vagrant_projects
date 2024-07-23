914c5d9ac5263b64da20331c2e2b413a8786f98c
mjordan79 <renato.perini@gmail.com>
2024-07-23 23:50:02 +0200

    Minor tweaks and updates

--- ubuntu2204_hyperv/scripts/bash/basic_software_provision.sh
+++ ubuntu2204_hyperv/scripts/bash/basic_software_provision.sh
@@ -4,4 +4,4 @@
 echo "Attempting to install basic software ...";
 apt update -yq && \
     apt install -yq net-tools network-manager policycoreutils policycoreutils-python-utils && \
-    localectl set-locale LANG=it_IT.UTF-8 && localectl set-x11-keymap it && snap refresh
+    localectl set-locale LANG=it_IT.UTF-8 && localectl set-x11-keymap it && snap refresh && apt autoclean
