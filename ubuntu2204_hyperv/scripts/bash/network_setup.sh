95f317c884ab2fa6a38fe8280d8eef345235f9d9
mjordan79 <renato.perini@gmail.com>
2025-11-24 04:12:21 +0100

    Refactoring how the switch is used. Fixing it in the vagrantfile and set a static IP without the need of rebooting

---
+++ ubuntu2204_hyperv/scripts/bash/network_setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Some parameters. Modify accordingly to the create-nat-hyperv-switch.ps1 script.
+IFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | head -n1)  # Find the network interface
+STATIC_IP="192.169.0.$(expr 20 + $1)/24" # The static IP we're going to set
+GATEWAY="192.169.0.1"       # Gateway
+DNS1="1.1.1.1"              # Primary DNS
+DNS2="8.8.8.8"              # Secondary DNS
+NETPLAN_FILE="/etc/netplan/01-netcfg.yaml"
+
+echo "[INFO] Configuring Netplan for $IFACE interface with static IP $STATIC_IP"
+
+# Backup of the existing file.
+if [ -f "$NETPLAN_FILE" ]; then
+    cp "$NETPLAN_FILE" "${NETPLAN_FILE}.bak"
+    echo "[INFO] Backup created in ${NETPLAN_FILE}.bak"
+fi
+
+# Write the new configuration
+cat << EOF > "$NETPLAN_FILE"
+network:
+  version: 2
+  ethernets:
+    $IFACE:
+      dhcp4: no
+      dhcp6: no
+      addresses:
+        - $STATIC_IP
+      routes:
+        - to: default
+          via: $GATEWAY
+      nameservers:
+        addresses: [$DNS1, $DNS2]
+EOF
+
+# Fix permissions on netplan files
+sudo chmod 600 /etc/netplan/*.yaml
+sudo chown root:root /etc/netplan/*.yaml
+
+# Apply the final configuration
+netplan apply
+
+echo "[INFO] Network configuration for static IP applied"
