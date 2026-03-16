7a92a13b59d9c2b08c3ee92bedb9df05ced04d13
mjordan79 <renato.perini@gmail.com>
2026-03-16 01:09:39 +0100

    Adding individual build scripts for Packer. Modified Vagrantfile for provisioning a full system

--- packer-vagrant/build_2204.sh
+++ packer-vagrant/build_2204.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 cd ubuntu
+# Build the image ...
 packer build -only ubuntu-hyperv-vagrant-22.04.hyperv-iso.ubuntu-server-2204 .
+# and import it into vagrant.
+vagrant box add --name digitalnucleus/ubuntu-server-22.04 --provider hyperv ./ubuntu-server-2204-lts.box
--- packer-vagrant/build_2404.sh
+++ packer-vagrant/build_2404.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 cd ubuntu
+# Build the image ...
 packer build -only ubuntu-hyperv-vagrant-24.04.hyperv-iso.ubuntu-server-2404 .
+# and import it into vagrant.
+vagrant box add --name digitalnucleus/ubuntu-server-24.04 --provider hyperv ./ubuntu-server-2404-lts.box
